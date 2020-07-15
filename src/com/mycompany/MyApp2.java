22d4a129e3d703c21360f7718f8f3da9eb1c11d5
Mirlan Ipasov <mipasov@gmail.com>
2020-07-16 01:10:25 +0600

    data types, variables, control statement, methods

---
+++ src/com/mycompany/MyApp2.java
@@ -0,0 +1,8 @@
+package com.mycompany;
+
+public class MyApp2 {
+
+    public static void main(String[] args){
+        System.out.println("My another class");
+    }
+}
---
+++ src/com/mypackage/MyFavoriteMovie.java
@@ -0,0 +1,8 @@
+package com.mypackage;
+
+public class MyFavoriteMovie {
+
+    public void printMyFavMovie(){
+        System.out.println("My Favorite movie is Man in Black!");
+    }
+}
