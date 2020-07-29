91e5e943a93a59a2744ebcac9002ec196fd933b6
Mirlan Ipasov <mipasov@gmail.com>
2020-07-30 01:02:29 +0600

    do while loop, and exercises

--- src/com/mycompany/DevxPicker.java
+++ src/com/mycompany/DevxPicker.java
@@ -1,3 +1,4 @@
+package com.mycompany;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -8,78 +9,86 @@ public class DevxPicker {
         final int NUMBER_OF_STUDENTS = 19;
 
         Scanner userInput = new Scanner(System.in);
+        boolean isValidInput = false;
+        int appMode = 1;
+        do {
+            System.out.println("Please select mode (1 - QA_StudsOnly, 2 - Game_ALL )");
+            if (userInput.hasNextInt()){
+                isValidInput = true;
+                appMode = userInput.nextInt();
+            } else {
+                isValidInput = false;
+            }
 
-        System.out.println("Please select mode (1 - QA_StudsOnly, 2 - Game_ALL )");
-        int appMode = userInput.nextInt();
+            Random picker = new Random();
+            int pickedNumber = picker.nextInt(NUMBER_OF_STUDENTS);
 
-        Random picker = new Random();
-        int pickedNumber = picker.nextInt(NUMBER_OF_STUDENTS);
-
-        switch (pickedNumber){
-            case 0:
-                System.out.println("Baktygul");
-                break;
-            case 1:
-                System.out.println("Irina B");
-                break;
-            case 2:
-                System.out.println("Jiyde");
-                break;
-            case 3:
-                System.out.println("Aiana");
-                break;
-            case 4:
-                System.out.println("Zalkar");
-                break;
-            case 5:
-                System.out.println("Azamat");
-                break;
-            case 6:
-                System.out.println("Irina R");
-                break;
-            case 7:
-                System.out.println("Ram");
-                break;
-            case 8:
-                System.out.println("Meruert");
-                break;
-            case 9:
-                System.out.println("Mirgul");
-                break;
-            case 10:
-                System.out.println("Elvina");
-                break;
-            case 11:
-                System.out.println("Almaz");
-                break;
-            case 12:
-                System.out.println("Kubah");
-                break;
-            case 13:
-                System.out.println("Luis");
-                break;
-            case 14:
-                if (appMode == 2) {
-                    System.out.println("Nuta");
+            switch (pickedNumber) {
+                case 0:
+                    System.out.println("Baktygul");
                     break;
-                }
-            case 15:
-                System.out.println("Gauhar");
-                break;
-            case 16:
-                if (appMode == 2) {
-                    System.out.println("Mirlan");
+                case 1:
+                    System.out.println("Irina B");
                     break;
-                }
-	    case 17:
-		    System.out.println("Oksana");
-		    break;
-            case 18:
-                System.out.println("Gulnaz");
-                break;
-            default:
-                System.out.println("Please try again!");
-                break;
-        }
+                case 2:
+                    System.out.println("Jiyde");
+                    break;
+                case 3:
+                    System.out.println("Aiana");
+                    break;
+                case 4:
+                    System.out.println("Zalkar");
+                    break;
+                case 5:
+                    System.out.println("Azamat");
+                    break;
+                case 6:
+                    System.out.println("Irina R");
+                    break;
+                case 7:
+                    System.out.println("Ram");
+                    break;
+                case 8:
+                    System.out.println("Meruert");
+                    break;
+                case 9:
+                    System.out.println("Mirgul");
+                    break;
+                case 10:
+                    System.out.println("Elvina");
+                    break;
+                case 11:
+                    System.out.println("Almaz");
+                    break;
+                case 12:
+                    System.out.println("Kubah");
+                    break;
+                case 13:
+                    System.out.println("Luis");
+                    break;
+                case 14:
+                    if (appMode == 2) {
+                        System.out.println("Nuta");
+                        break;
+                    }
+                case 15:
+                    System.out.println("Gauhar");
+                    break;
+                case 16:
+                    if (appMode == 2) {
+                        System.out.println("Mirlan");
+                        break;
+                    }
+                case 17:
+                    System.out.println("Oksana");
+                    break;
+                case 18:
+                    System.out.println("Gulnaz");
+                    break;
+                default:
+                    System.out.println("Please try again!");
+                    break;
+            }
+        } while (isValidInput);
     }
 }
---
+++ src/com/mycompany/LoopExsc2.java
@@ -0,0 +1,24 @@
+package com.mycompany;
+
+import java.util.Scanner;
+
+public class LoopExsc2 {
+
+    public static void main(String[] args){
+       Scanner userInput = new Scanner(System.in);
+       boolean isValidInput = false;
+       int a;
+       do {
+           System.out.println("Please enter Integer to continue, enter anything else to exit!");
+           if (userInput.hasNextInt()) {
+               isValidInput = true;
+               a = userInput.nextInt();
+               System.out.println("You got another chance!");
+           }else {
+               isValidInput = false;
+               System.out.println("No another chance, sorry!");
+           }
+       } while(isValidInput);
+
+    }
+}
