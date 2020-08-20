6ecbc18d43941bcd9bee3127c26bf3b42b0c94f1
Mirlan Ipasov <mipasov@gmail.com>
2020-08-21 01:03:09 +0600

    arrays, loops, arraylist exersises

---
+++ src/com/mycompany/DetectDuplicates.java
@@ -0,0 +1,20 @@
+package com.mycompany;
+
+public class DetectDuplicates {
+
+    public static void main(String[] args){
+        String arrString = "adhfhfhhhhhhhhasjfjfjaaaaaaa";
+        StringBuilder strB = new StringBuilder(arrString);
+        int duplicateCounter = 0;
+        for (int i = 0; i < strB.length(); i++ ){
+            duplicateCounter = 0;
+            for (int j = i + 1; j < strB.length(); j++){
+                if (strB.charAt(i) == strB.charAt(j)){
+                    duplicateCounter++;
+                }
+            }
+            strB.deleteCharAt(i);
+            System.out.println(strB.charAt(i)+ " has "+ duplicateCounter + "of duplicates");
+        }
+    }
+}
