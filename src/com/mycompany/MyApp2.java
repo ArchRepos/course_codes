1d11306ec5a21a79e95d24bcb9e6f82683173b4f
Mirlan Ipasov <mipasov@gmail.com>
2020-09-04 01:48:59 +0600

    files moved under meaningful packages

--- src/com/mycompany/MyApp2.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany;
-
-public class MyApp2 {
-
-    public static void main(String[] args){
-        System.out.println("My another class");
-    }
-}
--- src/com/mypackage/MyFavoriteMovie.java
+++
@@ -1,8 +0,0 @@
-package com.mypackage;
-
-public class MyFavoriteMovie {
-
-    public void printMyFavMovie(){
-        System.out.println("My Favorite movie is Man in Black!");
-    }
-}
