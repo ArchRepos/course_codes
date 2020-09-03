cbab36220834d138f77978a2fcfb174589f12ef7
Mirlan Ipasov <mipasov@gmail.com>
2020-09-04 01:51:32 +0600

    structure reorganize

---
+++ src/com/school/javacore/maps/HashMapLetterFreqExcs.java
@@ -0,0 +1,27 @@
+package com.school.javacore.maps;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class HashMapLetterFreqExcs {
+
+  public static void main(String[] args) {
+        HashMapLetterFreqExcs hashMapLetterFreqExcs = new HashMapLetterFreqExcs();
+        String s = "we are learning programming language java . java is good programming language";
+        System.out.println(hashMapLetterFreqExcs.calculateWordFrequencies(s).toString());
+  }
+
+  public HashMap<String, Integer> calculateWordFrequencies(String text){
+    HashMap<String, Integer> resultMap = new HashMap<>();
+    //"we are learning programming language java . java is good programming language"
+    String[] words = text.split(" ");
+    System.out.println(Arrays.toString(words));
+    for (String word: words) {
+      if (!resultMap.containsKey(word))
+        resultMap.put(word, 1);
+      else
+        resultMap.put(word, resultMap.get(word) + 1);
+    }
+    return resultMap;
+  }
+}
