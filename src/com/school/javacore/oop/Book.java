43ae64041d8f1a4dfd726c622542909048df46b6
Mirlan Ipasov <mipasov@gmail.com>
2020-09-10 23:38:44 +0600

    contsructors, getters setters, tests

--- src/com/school/javacore/oop/Book.java
+++ src/com/school/javacore/oop/Book.java
@@ -2,8 +2,29 @@ package com.school.javacore.oop;
 
 public class Book {
 
+    public Book(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
     private int price;
     private String name;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public int getPrice(String buyerType){
         if (buyerType == "student")
