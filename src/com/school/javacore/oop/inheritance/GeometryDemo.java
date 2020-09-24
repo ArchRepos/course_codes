f5b2a568d3b2b92cc0250f194539bf0c87daa2de
Mirlan Ipasov <mipasov@gmail.com>
2020-09-24 23:34:16 +0600

    person, staff, student

--- src/com/school/javacore/oop/inheritance/GeometryDemo.java
+++ src/com/school/javacore/oop/inheritance/GeometryDemo.java
@@ -4,7 +4,7 @@ public class GeometryDemo {
 
     public static void main(String[] args){
         Circle c = new Circle(3, "black");
-        Cylinder c1 = new Cylinder(5, "red", 8);
+        Circle c1 = new Cylinder(5, "red", 8);
 
         System.out.println(c);
         System.out.println(c1);
